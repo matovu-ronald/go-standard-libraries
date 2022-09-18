416027d37acae3a96ccb6649569afe4e8b94110b
Matovu Ronald <maronwhite6@gmail.com>
2022-09-18 12:29:58 +0300

    using Printf and Sprintf

--- formatting/main.go
+++ formatting/main.go
@@ -3,16 +3,42 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("This prints without adding a new line")
-	fmt.Println("This prints and adds a new line")
+	x := 10
+	y := 12.25
 
-	sum := 20
-	fmt.Println("This is sample number formatting option", sum)
-	const a, b, c = 5, 5, 10
-	fmt.Println("Add", a, "and", b, "to get", c)
+	// Basic formatting
+	fmt.Printf("%d\n", x)
+	fmt.Printf("%x\n", x)
 
-	// Printing a slice of values
-	items := []int{10, 20, 30, 40, 50}
-	length, err := fmt.Println(items)
-	fmt.Println(length, err)
+	// Print Booleans using "True" or "False"
+	fmt.Printf("%t\n", x > 10)
+
+	// Print floating point and exponential numbers
+	fmt.Printf("%f\n", y)
+	fmt.Printf("%e\n", y)
+
+	// Using explicit argument indexes
+	fmt.Printf("%[2]d %[1]d\n", 20, 23)
+
+	// Argument indexes can be used to print values repeatedly
+	fmt.Printf("%d %#[1]o %#[1]x\n", 15)
+
+	// Print a value in default format
+	c := circle{
+		radius: 7,
+		border: 2,
+	}
+
+	fmt.Printf("%+v\n", c)
+	fmt.Printf("%v\n", c)
+	fmt.Printf("%T\n", c)
+	// Sprintf works as Printf but returns a string
+	s := fmt.Sprintf("%[2]d %[1]d\n", 52, 40)
+	fmt.Println(s)
+
+}
+
+type circle struct {
+	radius int
+	border int
 }
